7e1793b6ccd692e7918f887acae157c296df5942
Jimmy Börjesson <alcesleo@gmail.com>
2017-04-13 17:48:33 +0200

    Clean up collision detection logic

--- lib/collision.rb
+++ lib/collision.rb
@@ -17,12 +17,10 @@ class Collision
   private
 
   def intersects?(rect1, rect2)
-    !(
-      rect2.position.x > (rect1.position.x + rect1.width) ||
-      (rect2.position.x + rect2.width) < rect1.position.x ||
-      rect2.position.y > (rect1.position.y + rect1.height) ||
-      (rect2.position.y + rect2.height) < rect1.position.y
-    )
+    rect1.position.x   <= rect2.position.x + rect2.width &&
+      rect2.position.x <= rect1.position.x + rect1.width &&
+      rect1.position.y <= rect2.position.y + rect2.height &&
+      rect2.position.y <= rect1.position.y + rect1.height
   end
 
   def detect_walls
